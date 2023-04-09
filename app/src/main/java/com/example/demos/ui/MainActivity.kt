f6ddcf739f3d1d37217fa24bdf319b22035db802
luqmanamirullah <amirullahluqman@gmail.com>
2023-04-10 00:26:32 +0700

    Initial commit

---
+++ app/src/main/java/com/example/demos/ui/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.demos.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.example.demos.R
+import com.example.demos.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navController = findNavController(R.id.frActivity)
+        binding.bottomNavigationView.setupWithNavController(navController)
+    }
+}
