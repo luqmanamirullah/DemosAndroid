ba31f03573195d073aab89ecb232e0cb00ab75cc
beilmouz <nutbill30@gmail.com>
2023-05-07 20:36:56 +0700

    feat: new login page merah

--- app/src/main/java/com/example/demos/ui/LoginActivity.kt
+++ app/src/main/java/com/example/demos/ui/LoginActivity.kt
@@ -2,6 +2,7 @@ package com.example.demos.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.demos.R
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
