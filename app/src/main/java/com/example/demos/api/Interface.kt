0dd70128cc7e66c307d89d504800b644a3020d56
luqmanamirullah <amirullahluqman@gmail.com>
2023-05-08 10:52:55 +0700

    refactor: change an endpoint

--- app/src/main/java/com/example/demos/api/Interface.kt
+++ app/src/main/java/com/example/demos/api/Interface.kt
@@ -6,7 +6,7 @@ import retrofit2.Response
 import retrofit2.http.GET
 
 interface Interface {
-    @GET("news")
+    @GET("breakingNews")
     suspend fun getNews() : Response<NewsLists>
 
     @GET("trending-lists")
--- app/src/main/java/com/example/demos/database/NewsDao.kt
+++ app/src/main/java/com/example/demos/database/NewsDao.kt
@@ -6,12 +6,7 @@ import com.example.demos.models.news.News
 
 @Dao
 interface NewsDao {
-    @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun upsert(news: News): Long
-
     @Query("SELECT * FROM news")
     fun getAllNews(): LiveData<List<News>>
 
-    @Delete
-    suspend fun hideNews(news: News)
 }
