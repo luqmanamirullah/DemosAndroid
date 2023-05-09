198103db8e40ca2acc624b6cb0c02adf67f7739b
luqmanamirullah <amirullahluqman@gmail.com>
2023-05-09 16:57:04 +0700

    feat: fetching search api

--- app/src/main/java/com/example/demos/api/Interface.kt
+++ app/src/main/java/com/example/demos/api/Interface.kt
@@ -1,14 +1,28 @@
 package com.example.demos.api
 
 import com.example.demos.models.news.NewsLists
+import com.example.demos.models.news.NewsType
 import com.example.demos.models.trending.TrendingLists
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface Interface {
     @GET("breakingNews")
-    suspend fun getNews() : Response<NewsLists>
+    suspend fun getNews(
+        @Query("type")
+        newsType: String? = null
+    ) : Response<NewsLists>
+
+    @GET("news-type")
+    suspend fun getNewsType() : Response<NewsType>
 
     @GET("trending-lists")
     suspend fun getTrends() : Response<TrendingLists>
+
+    @GET("everything")
+    suspend fun search(
+        @Query("q")
+        searchQuery: String = ""
+    ): Response<NewsLists>
 }
