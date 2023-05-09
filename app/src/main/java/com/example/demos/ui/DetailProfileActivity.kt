d3a74ae28dc2d601f7cecbd8e077fd67af9351e8
luqmanamirullah <amirullahluqman@gmail.com>
2023-05-09 16:54:14 +0700

    refactor

--- app/src/main/java/com/example/demos/ui/DetailProfileActivity.kt
+++ app/src/main/java/com/example/demos/ui/DetailProfileActivity.kt
@@ -1,14 +1,10 @@
 package com.example.demos.ui
 
-import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.widget.Toast
-import com.example.demos.R
 import com.example.demos.databinding.ActivityDetailProfileBinding
-import com.example.demos.ui.fragments.ProfileFragment
 
 class DetailProfileActivity : AppCompatActivity() {
     private lateinit var binding: ActivityDetailProfileBinding
