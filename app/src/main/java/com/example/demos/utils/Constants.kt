bf261ebd1f200f0f9f657d00ae1a4038048937f8
luqmanamirullah <amirullahluqman@gmail.com>
2023-05-09 16:53:26 +0700

    feat: Search delay

--- app/src/main/java/com/example/demos/utils/Constants.kt
+++ app/src/main/java/com/example/demos/utils/Constants.kt
@@ -3,5 +3,6 @@ package com.example.demos.utils
 class Constants {
     companion object {
         const val BASE_URL = "https://demosserve.000webhostapp.com/api/"
+        const val SEARCH_DELAY = 500L
     }
 }
