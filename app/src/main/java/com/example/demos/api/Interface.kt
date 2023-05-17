c4fca4e9dcf0ca28012bf403281da24bf3409546
luqmanamirullah <amirullahluqman@gmail.com>
2023-05-17 19:55:28 +0700

    feat: logout api

--- app/src/main/java/com/example/demos/api/Interface.kt
+++ app/src/main/java/com/example/demos/api/Interface.kt
@@ -1,5 +1,6 @@
 package com.example.demos.api
 
+import com.example.demos.models.Logout
 import com.example.demos.models.login.Login
 import com.example.demos.models.login.LoginRequest
 import com.example.demos.models.news.ArticleData
@@ -9,6 +10,7 @@ import com.example.demos.models.trending.TrendingLists
 import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.Header
 import retrofit2.http.POST
 import retrofit2.http.Path
 import retrofit2.http.Query
@@ -19,6 +21,11 @@ interface Interface {
     suspend fun login(
         @Body loginRequest: LoginRequest,
     ): Response<Login>
+
+    @POST("logout")
+    suspend fun logout(
+        @Header("Authorization") token : String
+    ): Response<Logout>
 
     @GET("breakingNews")
     suspend fun getNews(
