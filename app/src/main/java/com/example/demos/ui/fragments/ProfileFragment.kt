17d5954ec6a42e8396dbf63f0cbdca7101b7cd8e
luqmanamirullah <amirullahluqman@gmail.com>
2023-05-17 08:46:32 +0700

    feat: user data implementation

--- app/src/main/java/com/example/demos/ui/fragments/ProfileFragment.kt
+++ app/src/main/java/com/example/demos/ui/fragments/ProfileFragment.kt
@@ -1,6 +1,6 @@
 package com.example.demos.ui.fragments
 
-import android.app.Activity
+
 import android.app.AlertDialog
 import android.content.Intent
 import android.os.Bundle
@@ -8,21 +8,38 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
-import com.example.demos.R
-import com.example.demos.databinding.FragmentHomeBinding
+import com.bumptech.glide.Glide
 import com.example.demos.databinding.FragmentProfileBinding
+
 import com.example.demos.ui.*
+import com.example.demos.ui.viewmodels.LoginViewModel
+import com.example.demos.utils.SessionManager
 
 class ProfileFragment : Fragment() {
     private lateinit var binding: FragmentProfileBinding
+    private lateinit var viewModel: LoginViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         binding = FragmentProfileBinding.inflate(inflater, container, false)
+
+
+        SessionManager.getCurentUser(requireContext()).let {
+            binding.apply {
+                tvName.text = it.user_name
+                if (it.user_verify == 0) {
+                    tvJob.visibility = View.GONE
+                } else {
+                    tvJob.visibility = View.VISIBLE
+                }
+                tvEmail.text = it.user_email
+            }
+
+            Glide.with(this).load(it.user_picture).into(binding.ivPhotoProfile)
+        }
+
         binding.btnBack.setOnClickListener {
             requireActivity().onBackPressed()
         }
