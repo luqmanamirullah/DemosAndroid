5a4b93733096ce40d57d8968c0e0b87f2f3db230
luqmanamirullah <amirullahluqman@gmail.com>
2023-05-17 08:34:08 +0700

    feat: add key session

--- app/src/main/java/com/example/demos/utils/Constants.kt
+++ app/src/main/java/com/example/demos/utils/Constants.kt
@@ -7,6 +7,13 @@ class Constants {
         const val LOADING_VIEW_TYPE = 0
         const val DONE_VIEW_TYPE = 1
         const val MAX_STEP = 3
-
+        const val USER_TOKEN = "user_token"
+        const val USER_ID = "id"
+        const val USER_NAME = "nama"
+        const val USER_EMAIL = "email"
+        const val USER_VERIFY = "isVerify"
+        const val USER_PICTURE = "photoProfile"
+        const val USER_CREATED_AT = "createdAt"
+        const val USER_UPDATED_AT = "updatedAt"
     }
 }
