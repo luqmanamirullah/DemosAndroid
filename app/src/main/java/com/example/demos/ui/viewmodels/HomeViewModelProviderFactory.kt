60a0bfae65dfd957eacf7184c594af0ffe3b27ef
luqmanamirullah <amirullahluqman@gmail.com>
2023-05-21 19:23:39 +0700

    perf

--- app/src/main/java/com/example/demos/ui/viewmodels/HomeViewModelProviderFactory.kt
+++ app/src/main/java/com/example/demos/ui/viewmodels/HomeViewModelProviderFactory.kt
@@ -1,13 +1,14 @@
 package com.example.demos.ui.viewmodels
 
+import android.app.Application
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.example.demos.repository.HomeRepository
 
-class HomeViewModelProviderFactory(private val homeRepository: HomeRepository): ViewModelProvider.Factory {
+class HomeViewModelProviderFactory(val homeRepository: HomeRepository, val application: Application): ViewModelProvider.Factory {
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(HomeViewModel::class.java)){
-           return HomeViewModel(homeRepository) as T
+           return HomeViewModel(homeRepository, application) as T
         }
         throw IllegalArgumentException("Unknown ViewModel Class")
     }
