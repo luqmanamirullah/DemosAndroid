49b44f0cb994dc0ab594ed693b98c8499e87ad62
beilmouz <nutbill30@gmail.com>
2023-05-27 22:59:08 +0700

    feat: add Search Fragment

--- app/src/main/java/com/example/demos/models/newsFromInternet/Article.kt
+++ app/src/main/java/com/example/demos/models/newsFromInternet/Article.kt
@@ -2,19 +2,7 @@ package com.example.demos.models.newsFromInternet
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
-<<<<<<< HEAD
 import java.io.Serializable
-
-@Entity (
-    tableName = "news"
-        )
-data class Article(
-    @PrimaryKey (autoGenerate = true)
-    var id: Int? = null,
-    val author: String,
-    val content: String?,
-    val description: String?,
-=======
 
 @Entity (
     tableName = "news"
@@ -26,25 +14,15 @@ data class Article(
     val author: String,
     val content: String,
     val description: String,
->>>>>>> origin/news-app
     val publishedAt: String,
     val source: Source,
     val title: String,
     val url: String,
-<<<<<<< HEAD
     val urlToImage: String?
 ): Serializable {
-=======
-    val urlToImage: String
-){
->>>>>>> origin/news-app
     companion object {
         fun getFirstThreeArticles(articles: List<Article>): List<Article> {
             return articles.take(3)
         }
     }
-<<<<<<< HEAD
-=======
-
->>>>>>> origin/news-app
 }
